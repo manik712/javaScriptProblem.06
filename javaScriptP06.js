4fea6655a42db5e9eae171d1db8fddce82065153
manik712 <ranineela71@gmail.com>
2024-02-20 10:02:26 +0600

    add this

--- javaScriptP06.js
+++ javaScriptP06.js
@@ -1,1 +1,7 @@
+const numbers = [1, 3, 8, 7, 8, 1, 6, 4, 2,2];
 
+const result = numbers.filter(function(value, index, array) {
+  return array.indexOf(value) !== index;
+});
+
+console.log(result);
